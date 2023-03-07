cee722f52d5da85219cc7f68a8f1878d37c4bc50
Gary <Gary.Dix@enform.nz>
2023-03-08 12:05:47 +1300

    Pure method moved to CalcOperation class

---
+++ Operations/CalcOperations.cs
@@ -0,0 +1,29 @@
+﻿namespace CalculatorStudents.Operations
+{
+    public class CalcOperations
+    {
+
+        //this is a pure method!
+        public double Calculation(double NumberAd, double NumberBd, EnumOperationType OperationType)
+        {
+            if (OperationType == EnumOperationType.Addition)
+            {
+                return NumberAd + NumberBd;
+            }
+            else if (OperationType == EnumOperationType.Subtraction)
+            {
+                return NumberAd - NumberBd;
+            }
+            else if (OperationType == EnumOperationType.Division)
+            {
+                return NumberAd / NumberBd;
+            }
+            else if (OperationType == EnumOperationType.Multiplication)
+            {
+                return NumberAd * NumberBd;
+            }
+            return 0;
+        }
+
+    }
+}
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -24,6 +24,8 @@ namespace CalculatorStudents.Pages
 
         public EnumOperationType OperationType { get; set; }
 
+        public CalcOperations calc { get; set; }
+
         public void OnGet()
         {
 
@@ -34,24 +36,36 @@ namespace CalculatorStudents.Pages
             double NumberAd = Convert.ToDouble(NumberA);
             double NumberBd = Convert.ToDouble(NumberB);
 
-            if (OperationType == EnumOperationType.Addition)
-            {
-                Result = NumberAd + NumberBd;
-            }
-            else if (OperationType == EnumOperationType.Subtraction)
-            {
-                Result = NumberAd - NumberBd;
-            }
-            else if (OperationType == EnumOperationType.Division)
-            {
-                Result = NumberAd / NumberBd;
-            }
-            else if (OperationType == EnumOperationType.Multiplication)
-            {
-                Result = NumberAd * NumberBd;
-            }
+            Result = calc.Calculation(NumberAd, NumberBd, OperationType);
 
+
+            //357.345
+            //  Result = Calculation(123, 234.345, EnumOperationType.Addition);
 
         }
+
+        //this is a pure method!
+        //private double Calculation(double NumberAd, double NumberBd, EnumOperationType OperationType)
+        //{
+        //    if (OperationType == EnumOperationType.Addition)
+        //    {
+        //        return NumberAd + NumberBd;
+        //    }
+        //    else if (OperationType == EnumOperationType.Subtraction)
+        //    {
+        //        return NumberAd - NumberBd;
+        //    }
+        //    else if (OperationType == EnumOperationType.Division)
+        //    {
+        //        return NumberAd / NumberBd;
+        //    }
+        //    else if (OperationType == EnumOperationType.Multiplication)
+        //    {
+        //        return NumberAd * NumberBd;
+        //    }
+        //    return 0;
+        //}
+
+
     }
 }
