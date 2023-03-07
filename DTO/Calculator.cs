ff46eb20ecf0d9276f700203bde7483712a0dd78
Gary <Gary.Dix@enform.nz>
2023-03-08 12:25:22 +1300

    Moved properties to own class and updated calculations to match

---
+++ DTO/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using CalculatorStudents.Operations;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace CalculatorStudents.DTO
+{
+    public class Calculator
+    {
+        [Display(Name = "Enter First Number")]
+        public string? NumberA { get; set; }
+        [Display(Name = "Enter Second Number")]
+        public string? NumberB { get; set; }
+        [Display(Name = "Your Answer is...")]
+        public double Result { get; set; }
+
+        [Display(Name = "Choose and Operation")]
+        public string? Operation { get; set; }
+
+        public EnumOperationType OperationType { get; set; }
+
+
+    }
+}
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,71 +1,31 @@
 ﻿
 
+using CalculatorStudents.DTO;
 using CalculatorStudents.Operations;
 
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
-using System.ComponentModel.DataAnnotations;
 
 namespace CalculatorStudents.Pages
 {
     [BindProperties]
     public class IndexModel : PageModel
     {
-        [Display(Name = "Enter First Number")]
-        public string? NumberA { get; set; }
-        [Display(Name = "Enter Second Number")]
-        public string? NumberB { get; set; }
-        [Display(Name = "Your Answer is...")]
-        public double Result { get; set; }
-
-        [Display(Name = "Choose and Operation")]
-        public string? Operation { get; set; }
-
-        public EnumOperationType OperationType { get; set; }
-
+        //This calls in the calculations from the CalcOperations.cs
         public CalcOperations calc { get; set; }
 
-        public void OnGet()
-        {
-
-        }
+        //This calls in the properties from the Calculator.cs
+        public Calculator calculator { get; set; }
 
         public void OnPostAsync()
         {
-            double NumberAd = Convert.ToDouble(NumberA);
-            double NumberBd = Convert.ToDouble(NumberB);
+            //convert the string numbers to doubles for processing 
+            double NumberAd = Convert.ToDouble(calculator.NumberA);
+            double NumberBd = Convert.ToDouble(calculator.NumberB);
 
-            Result = calc.Calculation(NumberAd, NumberBd, OperationType);
-
-
-            //357.345
-            //  Result = Calculation(123, 234.345, EnumOperationType.Addition);
+            //run the calculation and return back the result
+            calculator.Result = calc.Calculation(NumberAd, NumberBd, calculator.OperationType);
 
         }
-
-        //this is a pure method!
-        //private double Calculation(double NumberAd, double NumberBd, EnumOperationType OperationType)
-        //{
-        //    if (OperationType == EnumOperationType.Addition)
-        //    {
-        //        return NumberAd + NumberBd;
-        //    }
-        //    else if (OperationType == EnumOperationType.Subtraction)
-        //    {
-        //        return NumberAd - NumberBd;
-        //    }
-        //    else if (OperationType == EnumOperationType.Division)
-        //    {
-        //        return NumberAd / NumberBd;
-        //    }
-        //    else if (OperationType == EnumOperationType.Multiplication)
-        //    {
-        //        return NumberAd * NumberBd;
-        //    }
-        //    return 0;
-        //}
-
-
     }
 }
